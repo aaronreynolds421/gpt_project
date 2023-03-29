c18dadbfa02c8717465fb8ab38265270dceabac8
aaronreynolds421 <aaronreynolds421@gmail.com>
2023-03-29 14:04:55 -0700

    update 4

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import {RiMenuLine, RiCloseLin } from 'react-icons/ri'
 
 import './navigation.css'
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <div className = "gpt4__navbar">
+      <div className='gpt4__navbar-links'>
+      <div className='gpt4__navbar-links_logo'>
+          <img src={logo} alt="logo"/>
+        </div> 
+      </div>
+    </div>
   )
 }
 
