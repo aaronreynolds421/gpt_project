ac199128afbce2905f025663116a35b1f294cd89
aaronreynolds421 <aaronreynolds421@gmail.com>
2023-03-29 11:35:31 -0700

    update 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import {  Header, Footer, Blog, Possibility, Features, WhatGPT } from './container'
 import { Navigation, Brand, Cta } from './components'
+import './App.css'
 
 function App() {
   return (
-    <div>
-        <h1>GPT-4</h1>
+    <div className="App">
+        <div className="gradient__bg">
+          <Navigation></Navigation>
+          <Header></Header>
+        </div>
+        <Brand></Brand>
+        <WhatGPT></WhatGPT>
+        <Features></Features>
+        <Possibility></Possibility>
+        <Cta></Cta>
+        <Blog></Blog>
+        <Footer></Footer>
     </div>
   )
 }
