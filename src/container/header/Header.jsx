3c9b06d296c11d98bcd13452520fa12896e857fb
aaronreynolds421 <aaronreynolds421@gmail.com>
2023-03-31 13:25:53 -0700

    update

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
@@ -12,6 +14,13 @@ const Header = () => {
           <input type="email" placeholder="Your email address" />
           <button type="button">Get Started</button>
         </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>500 people have requested access</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
