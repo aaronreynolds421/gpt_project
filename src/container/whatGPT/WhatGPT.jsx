132abe1aba3a0043d14d18e9740c05679dd9352e
aaronreynolds421 <aaronreynolds421@gmail.com>
2023-04-05 12:16:24 -0700

    whatgpt update

--- src/container/whatGPT/WhatGPT.jsx
+++ src/container/whatGPT/WhatGPT.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import './whatGPT.css'
+import "./whatGPT.css";
+import { Feature } from "../../components";
 
 const WhatGPT = () => {
   return (
-    <div>WhatGPT</div>
-  )
-}
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond imagination
+        </h1>
+        <p>Explore Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
+};
 
-export default WhatGPT
+export default WhatGPT;
