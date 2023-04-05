a958bab4104bb65c8f87b412330974ed7c15853f
aaronreynolds421 <aaronreynolds421@gmail.com>
2023-04-05 11:34:41 -0700

    update

--- src/components/brands/Brand.jsx
+++ src/components/brands/Brand.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { google, dropbox, slack, atlassian, shopify } from "./imports";
 
-import './brand.css'
+import "./brand.css";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google"></img>
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox"></img>
+      </div>
+      <div>
+        <img src={slack} alt="slack"></img>
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian"></img>
+      </div>
+      <div>
+        <img src={shopify} alt="shopify"></img>
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
